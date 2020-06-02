c7cec3b6b8b2d5e9658b0262c7c7a0ee849f1a74
Steve Anderson <prototype909@msn.com>
2020-06-02 21:31:16 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,7 +32,13 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT title
-   FROM series"
+   FROM series
+   INNER JOIN characters
+   ON series.id = characters.series_id
+   WHERE species = human
+   GROUP BY title
+   ORDER BY COUNT(species) DESC
+   LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
