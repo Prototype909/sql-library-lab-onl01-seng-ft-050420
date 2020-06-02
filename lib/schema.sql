687ba1fdd12b02945a2a9c024b167c6506951f25
Steve Anderson <prototype909@msn.com>
2020-06-02 20:29:33 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -14,22 +14,22 @@ CREATE TABLE authors (
   id INTEGER PRIMARY KEY,
   name TEXT
   );
-  
+
 CREATE TABLE books (
   id INTEGER PRIMARY KEY,
-  title TEXT, 
-  year INTEGER, 
-  series_id INTEGER 
+  title TEXT,
+  year INTEGER,
+  series_id INTEGER
   );
-  
+
 CREATE TABLE characters (
   id INTEGER PRIMARY KEY,
-  name TEXT, 
-  motto TEXT, 
-  species TEXT, 
-  author_id INTEGER 
+  name TEXT,
+  motto TEXT,
+  species TEXT,
+  author_id INTEGER
   );
-  
+
 CREATE TABLE character_books (
   id INTEGER PRIMARY KEY,
   character_id INTEGER,
