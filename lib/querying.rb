9e5195a949d9c9faa7f59a410c118caa917bfba1
Steve Anderson <prototype909@msn.com>
2020-06-02 21:23:12 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -25,7 +25,9 @@ def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
    FROM authors
    INNER JOIN series
-   ON authors.id = series.author_id"
+   ON authors.id = series.author_id
+   INNER JOIN subgenres
+   ON subgenre.id = series.subgenre_id"
 end
 
 def select_series_title_with_most_human_characters
